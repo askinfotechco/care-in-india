2d16ed7a843d0d96be42109581cacf4c8270390f
lord-vector <r.anuj.agrawal@gmail.com>
2023-11-25 01:51:50 +0530

    adding home page part 1

---
+++ front-end/src/molecules/featureIcons.js
@@ -0,0 +1,17 @@
+// import React from "react";
+
+// export default function FeatureIcons(props) {
+//   return (
+//     <div>
+//       <MdVideoCall style={{ height: 50, width: 50, cursor: "pointer" }} />
+//       <Label
+//         style={{
+//           fontSize: "12px",
+//           color: "rgba(0, 0, 0, 0.6)",
+//         }}
+//       >
+//         {"Schedule a video call"}
+//       </Label>
+//     </div>
+//   );
+// }
