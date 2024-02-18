5ebc7525098c65e3b52da28b9ffe86c15110aea9
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2024-02-18 22:35:12 +0530

    navbar issue resolved

--- frontend/src/molecules/navBar.js
+++ frontend/src/molecules/navBar.js
@@ -46,12 +46,11 @@ export default function NavBarTop() {
           <span className="navbar-toggler-icon"></span>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="hidden sm:ml-6 sm:flex sm:space-x-8" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto mr-auto">
             <li className="nav-item active ">
               <a className="nav-link p-0 text-white" href="/">
                 Home
-                {/* <span className="sr-only">(current)</span> */}
               </a>
             </li>
             <li className="nav-item">
@@ -80,7 +79,7 @@ export default function NavBarTop() {
           {userEmail === null ? (
             <>
               <span>
-                <Link className="navbar-btn text-white" to={"/signin"}>
+                <Link className="navbar-btn text-white ml-60" to={"/signin"}>
                   {"Login"}
                 </Link>
               </span>
